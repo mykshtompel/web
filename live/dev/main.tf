9e5b3704148f80b9f875e3512592daca169d5031
unknown <mykola.shtompel@gmail.com>
2021-08-25 11:48:46 +0300

    first

--- live/dev/main.tf
+++ live/dev/main.tf
@@ -44,7 +44,7 @@ module "codebuild" {
   repo_url           = var.repo_url
   branch_pattern     = var.branch_pattern
   git_trigger_event  = var.git_trigger_event
-  build_spec_file    = "${path.root}/config/buildspec_dev.yml"
+  build_spec_file    = "${path.root}/live/dev/config/buildspec_dev.yml"
   app                = var.app
   env                = var.env
   name_container     = var.name_container
